a3c6ac7c15389e90390b81526ed74ec536974ba8
Jaewon Han <nownah90@gmail.com>
2015-09-19 16:40:26 +0000

    mv

--- public/js/abc.js
+++ public/js/abc.js
@@ -1,170 +1,170 @@
 function choice(selvalue) {
 if(selvalue == "0"){
-var newSrc = '/assets/cover_main';
+var newSrc = 'assets/cover_main.jpg';
 }else if(selvalue == "1"){
-var newSrc = '/assets/gachon.pdf';
+var newSrc = '/assets/cover_gachon.jpg';
 }
 else if(selvalue == "2"){
-var newSrc = '/assets/cover_catholic';
+var newSrc = '/assets/cover_catholic.jpg';
 }
 else if(selvalue == "3"){
-var newSrc = '/public/assets/cover_kangwon';
+var newSrc = 'assets/cover_kangwon.jpg';
 }
 else if(selvalue == "4"){
-var newSrc = '/assets/cover_konkuk';
+var newSrc = '/assets/cover_konkuk.jpg';
 }
 else if(selvalue == "5"){
-var newSrc = '/assets/cover_kyonggi';
+var newSrc = '/assets/cover_kyonggi.jpg';
 }
 else if(selvalue == "6"){
-var newSrc = '/assets/cover_kyungdong';
+var newSrc = '/assets/cover_kyungdong.jpg';
 }    
 else if(selvalue == "7"){
-var newSrc = '/assets/cover_kyungpook';
+var newSrc = '/assets/cover_kyungpook.jpg';
 }
 else if(selvalue == "8"){
-var newSrc = '/assets/cover_kyungil';
+var newSrc = '/assets/cover_kyungil.jpg';
 }
 else if(selvalue == "9"){
-var newSrc = '/assets/cover_kyunghee';
+var newSrc = '/assets/cover_kyunghee.jpg';
 }
 else if(selvalue == "10"){
-var newSrc = '/assets/cover_keimyung';
+var newSrc = '/assets/cover_keimyung.jpg';
 }
 else if(selvalue == "11"){
-var newSrc = '/assets/cover_keimyungcollege';
+var newSrc = '/assets/cover_keimyungcollege.jpg';
 }
 else if(selvalue == "12"){
-var newSrc = '/assets/cover_korea';
+var newSrc = '/assets/cover_korea.jpg';
 }
 else if(selvalue == "13"){
-var newSrc = '/assets/cover_kwangwoon';
+var newSrc = '/assets/cover_kwangwoon.jpg';
 }
 else if(selvalue == "14"){
-var newSrc = '/assets/cover_kwangjuwomen';
+var newSrc = '/assets/cover_kwangjuwomen.jpg';
 }
 else if(selvalue == "15"){
-var newSrc = '/assets/cover_kookmin';
+var newSrc = '/assets/cover_kookmin.jpg';
 }
 else if(selvalue == "16"){
-var newSrc = '/assets/cover_dankook';
+var newSrc = '/assets/cover_dankook.jpg';
 }
 else if(selvalue == "17"){
-var newSrc = '/assets/cover_daeguhealth';
+var newSrc = '/assets/cover_daeguhealth.jpg';
 }
 else if(selvalue == "18"){
-var newSrc = '/assets/cover_daelim';
+var newSrc = '/assets/cover_daelim.jpg';
 }
 else if(selvalue == "19"){
-var newSrc = '/assets/cover_dongguk';
+var newSrc = '/assets/cover_dongguk.jpg';
 }
 else if(selvalue == "20"){
-var newSrc = '/assets/cover_dongeui';
+var newSrc = '/assets/cover_dongeui.jpg';
 }
 else if(selvalue == "21"){
-var newSrc = '/assets/cover_myongji';
+var newSrc = '/assets/cover_myongji.jpg';
 }
 else if(selvalue == "22"){
-var newSrc = '/assets/cover_baekseok';
+var newSrc = '/assets/cover_baekseok.jpg';
 }
 else if(selvalue == "23"){
-var newSrc = '/assets/cover_busan';
+var newSrc = '/assets/cover_busan.jpg';
 }   
 else if(selvalue == "24"){
-var newSrc = '/assets/cover_bucheon';
+var newSrc = '/assets/cover_bucheon.jpg';
 }
 else if(selvalue == "25"){
-var newSrc = '/assets/cover_sahmyook';
+var newSrc = '/assets/cover_sahmyook.jpg';
 }
 else if(selvalue == "26"){
-var newSrc = '/assets/cover_sogang';
+var newSrc = '/assets/cover_sogang.jpg';
 }    
 else if(selvalue == "27"){
-var newSrc = '/assets/cover_seoulsciencetechnology';
+var newSrc = '/assets/cover_seoulsciencetechnology.jpg';
 }
 else if(selvalue == "28"){
-var newSrc = '/assets/cover_seouleducation';
+var newSrc = '/assets/cover_seouleducation.jpg';
 }
 else if(selvalue == "29"){
-var newSrc = '/assets/cover_seoulcity';
+var newSrc = '/assets/cover_seoulcity.jpg';
 }    
 else if(selvalue == "30"){
-var newSrc = '/assets/cover_seoulwomen';
+var newSrc = '/assets/cover_seoulwomen.jpg';
 }
 else if(selvalue == "31"){
-var newSrc = '/assets/cover_sungkyul';
+var newSrc = '/assets/cover_sungkyul.jpg';
 }
 else if(selvalue == "32"){
-var newSrc = '/assets/cover_sungshin';
+var newSrc = '/assets/cover_sungshin.jpg';
 }
 else if(selvalue == "33"){
-var newSrc = '/assets/cover_suwon';
+var newSrc = '/assets/cover_suwon.jpg';
 }
 else if(selvalue == "34"){
-var newSrc = '/assets/cover_sookmyungwomen';
+var newSrc = '/assets/cover_sookmyungwomen.jpg';
 }
 else if(selvalue == "35"){
-var newSrc = '/assets/cover_soongsil';
+var newSrc = '/assets/cover_soongsil.jpg';
 }
 else if(selvalue == "36"){
-var newSrc = '/assets/cover_shingu';
+var newSrc = '/assets/cover_shingu.jpg';
 }
 else if(selvalue == "37"){
-var newSrc = '/assets/cover_andongscience';
+var newSrc = '/assets/cover_andongscience.jpg';
 }  
 else if(selvalue == "38"){
-var newSrc = '/assets/cover_yeonsung';
+var newSrc = '/assets/cover_yeonsung.jpg';
 }
 else if(selvalue == "39"){
-var newSrc = '/assets/cover_yonsei';
+var newSrc = '/assets/cover_yonsei.jpg';
 }    
 else if(selvalue == "40"){
-var newSrc = '/assets/cover_yeungnam';
+var newSrc = '/assets/cover_yeungnam.jpg';
 }    
 else if(selvalue == "41"){
-var newSrc = '/assets/cover_wonkwang';
+var newSrc = '/assets/cover_wonkwang.jpg';
 }    
 else if(selvalue == "42"){
-var newSrc = '/assets/cover_ewhawoman';
+var newSrc = '/assets/cover_ewhawoman.jpg';
 }    
 else if(selvalue == "43"){
-var newSrc = '/assets/cover_incheon';
+var newSrc = '/assets/cover_incheon.jpg';
 }    
 else if(selvalue == "44"){
-var newSrc = '/assets/cover_inha';
+var newSrc = '/assets/cover_inha.jpg';
 }    
 else if(selvalue == "45"){
-var newSrc = '/assets/cover_chonbuk';
+var newSrc = '/assets/cover_chonbuk.jpg';
 }    
 else if(selvalue == "46"){
-var newSrc = '/assets/cover_jeonju';
+var newSrc = '/assets/cover_jeonju.jpg';
 }    
 else if(selvalue == "47"){
-var newSrc = '/assets/cover_chungang';
+var newSrc = '/assets/cover_chungang.jpg';
 }    
 else if(selvalue == "48"){
-var newSrc = '/assets/cover_chungbuk';
+var newSrc = '/assets/cover_chungbuk.jpg';
 }
 else if(selvalue == "49"){
-var newSrc = '/assets/cover_hankukforeign';
+var newSrc = '/assets/cover_hankukforeign.jpg';
 }    
 else if(selvalue == "50"){
-var newSrc = '/assets/cover_hannam';
+var newSrc = '/assets/cover_hannam.jpg';
 }    
 else if(selvalue == "51"){
-var newSrc = '/assets/cover_hansei';
+var newSrc = '/assets/cover_hansei.jpg';
 }    
 else if(selvalue == "52"){
-var newSrc = '/assets/cover_hanshin';
+var newSrc = '/assets/cover_hanshin.jpg';
 }
 else if(selvalue == "53"){
-var newSrc = '/assets/cover_hanyang';
+var newSrc = '/assets/cover_hanyang.jpg';
 } 
 else if(selvalue == "54"){
-var newSrc = '/assets/cover_hyupsung';
+var newSrc = '/assets/cover_hyupsung.jpg';
 }
 else if(selvalue == "55"){
-var newSrc = '/assets/cover_hongik';
+var newSrc = '/assets/cover_hongik.jpg';
 }
 
 document.getElementById('wfrm').src = newSrc;
